27a96b1624931ee5bca28956981c9f5bf8100a8a
Vale González <valeysa30@hotmail.com>
2021-12-08 14:33:34 -0600

    Extract class and move method refactorings implemented

--- src/model/WorldGenerator.java
+++ src/model/WorldGenerator.java
@@ -4,13 +4,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Set;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class WorldGenerator {
+    Validation validate = new Validation();
     private static final String PANEL = ("" +
             "░░░░░░░░░░░░░░░░░░░░░░░░░\n" +
             "░░░░░░░░░░░░░░░░░░░░░░░░░\n" +
@@ -55,67 +55,29 @@ public class WorldGenerator {
     }
 
     private boolean validateFileStructure(JSONObject json) {
-        try {
-            return (validateUpperLevelOptions(json) && validateLength(json) &&
-                    validateConnections(json));
-        } catch (Exception e) {
-            return false;
-        }
+        return validate.validateFileStructure(json);
     }
 
-    private boolean validateUpperLevelOptions(JSONObject json) {
-        Set<String> upperLevel = json.keySet();
-        return (upperLevel.contains("Rooms") && upperLevel.contains("Main") && upperLevel.contains("Connections")
-                && json.get("Rooms") != null && json.get("Main") != null && json.get("Connections") != null);
-    }
+    // private boolean validateUpperLevelOptions(JSONObject json) {
+    // return validate.validateUpperLevelOptions(json);
+    // }
 
-    private boolean validateLength(JSONObject json) {
-        JSONArray declaredRooms = (JSONArray) json.get("Rooms");
-        Set<String> declaredConnections = ((JSONObject) json.get("Connections")).keySet();
-        return declaredRooms.size() == declaredConnections.size() && declaredConnections.size() > 0;
-    }
+    // private boolean validateLength(JSONObject json) {
+    // return validate.validateLength(json);
+    // }
 
-    private boolean validateConnections(JSONObject json) {
-        JSONObject connections = (JSONObject) json.get("Connections");
-        JSONArray declaredRooms = (JSONArray) json.get("Rooms");
-        Set<String> roomsLevel = connections.keySet();
+    // private boolean validateConnections(JSONObject json) {
+    // return validate.validateConnections(json);
+    // }
 
-        if (!declaredRooms.contains((String) json.get("Main"))) {
-            return false;
-        }
+    // private boolean validateOrientations(Set<String> orientationKeys) {
+    // return validate.validateOrientations(orientationKeys);
+    // }
 
-        for (String roomName : roomsLevel) {
-            JSONObject room = (JSONObject) connections.get(roomName);
-            Set<String> orientationKeys = room.keySet();
-
-            if (!(declaredRooms.contains(roomName) && validateOrientations(orientationKeys) &&
-                    validatePaths(room, orientationKeys, declaredRooms))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private boolean validateOrientations(Set<String> orientationKeys) {
-        return (orientationKeys.size() == 4 && orientationKeys.contains("North") &&
-                orientationKeys.contains("South") && orientationKeys.contains("East") &&
-                orientationKeys.contains("West"));
-    }
-
-    private boolean validatePaths(JSONObject room, Set<String> orientationKeys, JSONArray declaredRooms) {
-        for (String path : orientationKeys) {
-            String destination = (String) room.get(path);
-
-            if (destination != null) {
-                if (!declaredRooms.contains(destination)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
+    // private boolean validatePaths(JSONObject room, Set<String> orientationKeys,
+    // JSONArray declaredRooms) {
+    // return validate.validatePaths(room, orientationKeys, declaredRooms);
+    // }
 
     private HashMap<String, StateRoom> generateRoomsMap(JSONObject json) {
         HashMap<String, StateRoom> rooms = new HashMap<>();
